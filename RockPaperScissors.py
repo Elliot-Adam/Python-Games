2aa135af552dff0ab52270a847de70fa7c17c703
goldroger12389 <thefeircestryker@gmail.com>
2023-02-19 17:36:42 -0500

    Made rps and TicTacToe their own files

---
+++ RockPaperScissors.py
@@ -0,0 +1,231 @@
+#Rock Paper Scisssors
+#Imports
+import random
+import time
+
+#Ascii art
+def rock():
+    return'''
+           ,--.--._     
+    ------" _, \___)    
+            / _/____)   
+            \ /(____)   
+    ------\     (__)    
+            `-----"     
+    '''
+
+def scissors():
+    return'''
+                        
+     __       ,/'        
+    (__).  ,/'           
+     __  ::              
+    (__)'  `\.           
+              `\.        
+    '''    
+
+def paper():
+    return'''
+            _________          
+           /        /          
+          /        /           
+         /        /            
+        /________/             
+                            
+    '''
+#Prints
+def countdown():
+    for i in [3,2,1]:
+        Delay(0.5)
+        print(i)
+
+def errormes():
+    print("\nPlease input your selection of 1, 2 ,or 3\n")
+
+def Delay(amount):
+    time.sleep(amount)
+
+def Chose(parameter):
+    print(parameter,'chose\n')
+    Delay(0.25)
+    
+def Score(score1,score2):
+    print('Player Score: {} Computer Score: {}'.format(score1,(score2 * -1)))
+
+def Winner(winner):
+    print (winner, ("wins"))
+
+def image(image1,image2):
+    splitimageleft = image1().split('\n')
+    splitimageright = invert(image2()).split('\n')
+    smallerImage = min(len(splitimageleft),len(splitimageright))
+    for i,_ in enumerate(range(0,smallerImage)):
+        print(splitimageleft[i] + splitimageright[i])
+
+
+#Invert Ascii Art
+def turnAround(strng:str):
+    return strng[::-1]
+
+def invert(shape:str):
+    splitShape = shape.split('\n')
+    newShapeList = []
+    for line in splitShape:
+        revline = turnAround(line)
+        checkedRevLine = invertCharc(revline)
+        newShapeList.append(checkedRevLine)
+        finalShapeList = "\n".join(newShapeList)
+    return finalShapeList
+
+def invertCharc(string:str):
+    table = string.maketrans(r'()[]{}<>/\\',r')(][}{><\\/')
+    string = string.translate(table)
+    return string
+
+
+#Inputs
+def scoreGetter():
+    while True:
+        breaker = False
+        scoreGoalValid = [1,3,5]
+        scoreGoalInput = input("Best of 1 , 3 , 5\n")
+        try:
+            int(scoreGoalInput)
+        except ValueError:
+            print("\nCannot use letters\n")
+            continue
+        if int(scoreGoalInput) in scoreGoalValid:
+            intscoreGoalInput = int(scoreGoalInput)
+            breaker = True 
+        else:
+            print("\nInvalid\n")
+        if breaker:
+            return [intscoreGoalInput,(intscoreGoalInput * -1)]
+
+def Player_choice():
+    while True:
+        pc = input("Choose\n1. Rock\n2. Paper\n3. Scissors\n")
+        print ("\n")
+        if len(pc) > 1:
+            errormes()
+            continue
+        try:
+            pci = int(pc)
+        except:
+            errormes()
+            continue
+        if not pci in [1,2,3]:
+            errormes()
+            continue
+
+        if pci == 1: 
+            return 'pcr'
+        elif pci == 2:
+            return 'pcp'
+        elif pci == 3:
+            return 'pcs'
+
+def AI_choice(numa):
+    ac = random.choice(numa)
+    Delay(0.75)
+    if ac == 1:
+        return 'acr'
+    if ac == 2:
+        return 'acp'
+    if ac == 3:
+        return 'acs'
+
+def finishvars(search):
+    match search:
+        case 'rr':
+            image(rock,rock)
+            return 't'
+        case 'rs':
+            image(rock,scissors)
+            return 'w'
+        case 'rp':
+            image(rock,paper)
+            return 'l'
+        case 'ss':
+            image(scissors,scissors)
+            return 't'
+        case 'sp':
+            image(scissors,paper)
+            return 'w'
+        case 'sr': 
+            image(scissors,rock)
+            return 'l'
+        case 'pp':
+            image(paper,paper)
+            return 't'
+        case 'pr':
+            image(paper,rock)
+            return 'w'
+        case 'ps':
+            image(paper,scissors)
+            return 'l'
+
+def doneprompt():
+    z = input("Do you want to play again\nYes\nNo\n")
+    if z == "Yes" or z == "yes":
+        return False
+    if z == "No" or z == "no":
+        print ("Thanks for Playing")
+        return True
+    print("Invalid\n")
+
+def combinations(pchoice,achoice):
+    countdown()
+    outcome = finishvars(pchoice[2] + achoice[2])
+    if outcome == 'w':
+        Winner('Player')
+        return 'p'
+    if outcome == 'l':
+        Winner('Computer')
+        return 'a'
+    if outcome == 't':
+        print('Tie Game')
+        return 't'
+
+def scoreCheck(score):
+    if score in scoreGoal:
+        if score > 0:
+            return setWinner('Player')
+        elif score < 0:
+            return setWinner('Computer')
+    else:
+        return 'f'
+    
+def setWinner(setwinner):
+    print (setwinner, "wins the set")
+    dp = doneprompt()
+    return dp
+
+if __name__ == '__main__':
+    donePlaying = False
+    while not donePlaying:
+        pscore = 0
+        ascore = 0
+        scoreGoal = scoreGetter()
+        while True:
+            player_decision = Player_choice()
+            Chose('Player')
+            AI_decision = AI_choice([1,2,3])
+            Chose('Computer')
+            finalOutcome = combinations(player_decision,AI_decision)
+            if finalOutcome == 't':
+                continue
+            if finalOutcome == 'p':
+                pscore += 1
+            if finalOutcome == 'a':
+                ascore -= 1
+            Score(pscore,ascore)
+            Delay(0.7)
+            donePlaying = scoreCheck(ascore)
+            donePlaying = scoreCheck(pscore)
+            if not donePlaying == 'f':
+                if not donePlaying:
+                    pscore = 0
+                    ascore = 0
+                else:
+                    break
---
+++ TicTacToe.py
@@ -0,0 +1,458 @@
+#Tic Tac Toe
+#Imports 
+import random
+
+#Classes
+class Board:
+    def __init__(self):
+        self.strstart = '\x1b['
+        self.strred = '31'
+        self.strgreen = '32'
+        self.strwhite = '37'
+        self.strblackbg = ';40m'
+        self.strundline = '4;'
+        self.strbold = '1;'
+        self.strclose = '0m'
+        self.spotlist = []
+        self.donePlaying = False
+        global wcx
+        global wco
+        self.winner = None
+        wcx = False
+        wco = False
+
+    def donePrompt(self):
+        while True:
+            dp = input('Do you want to play again.  ')
+            if dp == 'Yes' or dp == 'yes':
+                self.donePlaying = False
+                break
+            elif dp == 'No' or dp == 'no':
+                self.donePlaying = True
+                break
+            else:
+                print('\nPlease input "Yes" or "No".\n')
+        
+    def setBoard(self):
+        self.spotlist = []
+        for num in range(1,10):
+            self.spotlist.append(num)
+
+    def repCheck(self):
+        tempspotlist = list(set(self.spotlist))
+        if '0' in tempspotlist:
+            tempspotlist.remove('0')
+        if 'X' in tempspotlist:
+            tempspotlist.remove('X')
+        return tempspotlist
+
+    def change(self,num,letter):
+        if num + 1 in self.spotlist:
+            self.spotlist[num] = letter
+
+    def printBoard(self):
+        print('\n')
+        count = 0
+        for numindex in range(9):
+            listval = self.spotlist[numindex]
+            if not count == 2:
+                end = True
+            else:
+                end = False
+            if not wcx or wco:
+                #Check if spot has been chosen or not
+                if type(listval) == int:
+                    if end:
+                        #Check mid of line
+                        print(self.strstart + self.strundline + self.strwhite + self.strblackbg + str(listval) + self.strstart + self.strclose,end='_|_')
+                    else:
+                        #Check end of line
+                        print(self.strstart + self.strundline + self.strwhite + self.strblackbg + str(listval) + self.strstart + self.strclose)
+                else:
+                    #Prints red and bolded if prints letter
+                    if end:
+                        #Checks mid of line
+                        print(self.strstart + self.strundline + self.strbold + self.strred + self.strblackbg + str(listval) + self.strstart + self.strclose, end= '_|_')
+                    else:
+                        #Checks end of line
+                        print(self.strstart + self.strundline + self.strbold + self.strred + self.strblackbg + str(listval) + self.strstart + self.strclose)
+            else:
+                #prints green letters on win because direct list has already been changed to the green letters 
+                if end:
+                    print(listval,end='_|_')
+                else:
+                    print(listval)
+            if end:                
+                count += 1
+            else:
+                count = 0
+
+    def endCheck_front(self,playerX,playerO):
+        wcx,wco = self.winCheck_back_main()
+        if len(winningNums) > 1:
+            for winningNum in winningNums:
+                self.spotlist[winningNum] = self.strstart + self.strundline + self.strbold + self.strgreen + self.strblackbg + self.spotlist[winningNum] + self.strstart + self.strclose
+            if wcx:
+                self.winCheck_wrapup('X',playerX)
+                wcx = False
+                return self.donePlaying
+
+            if wco:
+                self.winCheck_wrapup('0',playerO)
+                wco = False
+                return self.donePlaying
+        
+        if self.tieCheck():
+            self.printBoard()
+            self.setBoard()
+            print('\nTie Game\n')
+            scorePrint()
+            self.donePrompt()
+            return self.donePlaying
+        return None
+
+    def winCheck_wrapup(self,letter,player):
+        self.winner = letter
+        self.printBoard()
+        self.setBoard()
+        print('\n{} has won this round\n'.format(player.name))
+        player.scoreIncrement()
+        scorePrint()
+        self.donePrompt()
+            
+    def winCheck_back_main(self):
+        global winningNums
+        winx = False
+        wino = False
+        winningNums = []
+        #Row Check
+        for row in range(3): 
+            if self.spotlist[row*3] == self.spotlist[row*3+1] == self.spotlist[row*3+2]: 
+                if self.spotlist[row*3+1] == 'X':
+                    winx = True
+                    wino = False
+                    winningNums = [row*3, row*3+1, row*3+2]
+                    break
+                if self.spotlist[row*3+1] == '0':
+                    winx = False
+                    wino = True
+                    winningNums = [row*3, row*3+1, row*3+2]
+                    break
+        #Column Check
+        for column in range(3):
+            if self.spotlist[column] == self.spotlist[column+3] == self.spotlist[column+6]:
+                if self.spotlist[column] == 'X':
+                    winx = True
+                    wino = False
+                    winningNums = [column,column+3,column+6]
+                    break
+                if self.spotlist[column] == '0':                    
+                    winx = False
+                    wino = True
+                    winningNums = [column,column+3,column+6]
+                    break
+        #Diagonal top left to bottom right check        
+        if self.spotlist[0] == self.spotlist[4] == self.spotlist[8]:
+            if self.spotlist[0] == 'X':
+                winx = True
+                wino = False
+            if self.spotlist[0] == '0':
+                winx = False
+                wino = True
+            winningNums = [0,4,8]
+        #Diagonal top right to bottom left check
+        if self.spotlist[2] == self.spotlist[4] == self.spotlist[6]:
+            if self.spotlist[2] == 'X':
+                winx = True
+                wino = False
+            if self.spotlist[2] == '0':
+                winx = False
+                wino = True
+            winningNums = [2,4,6]
+        return winx,wino     
+                    
+    def tieCheck(self):
+        if len(self.repCheck()) == 0:
+        #If tie
+            return True
+        return False
+
+    def __del__(self):
+        print('\nThanks for playing')
+
+class Player:
+    def __init__(self,name,letter):
+        self.score = 0
+        self.name = name
+        self.letter = letter
+        self.choice = None
+
+    def choiceToBoard(self):
+        board.change(self.choice,self.letter)
+    
+    def scoreIncrement(self):
+        if board.winner == self.letter:
+            self.score += 1
+            board.winner = None
+
+class Person(Player):
+    def choiceGetter(self):
+        while True:
+            board.printBoard()
+            self.choice = input('\n\n{}, Choose a spot\n'.format(self.name))
+            try:
+                self.choice = int(self.choice)
+            except:
+                print('\nGive a viable number\n\n')
+                continue
+            if self.choice not in board.repCheck():
+                print('\nGive a viable number\n\n')
+                continue
+            self.choice -= 1
+            break
+        
+class EasyAI(Player):
+    def choiceGetter(self):
+        self.choice = random.choice(board.repCheck())
+        self.choice -= 1
+
+class HardAI(Player):
+    def choiceGetter(self):
+        self.choice = self.optimalMove()
+        self.choice -= 1
+
+    def winningMove(self):
+        #check rows
+        winningMoves = []
+        for row in range(3):
+                row *= 3
+                #checks every combination
+                for num1,num2,num3 in [0,1,2],[1,2,0],[2,0,1]:
+                    if board.spotlist[row + num1] == board.spotlist[row + num2] == self.letter and type(board.spotlist[row + num3]) == int:
+                        winningMoves.append(board.spotlist[row + num3])
+        #checks columns
+        for column in range(3):
+            #checks every combination
+            for num1,num2,num3 in [0,3,6],[3,6,0],[6,0,3]:
+                    if board.spotlist[column + num1] == board.spotlist[column + num2] == self.letter and type(board.spotlist[column + num3]) == int:
+                        winningMoves.append(board.spotlist[column + num3])
+        #checks diagonals
+    
+        #checks first diagonal
+        for num1,num2,num3 in [0,4,8],[4,8,0],[8,0,4]:
+            if board.spotlist[num1] == board.spotlist[num2] == self.letter and type(board.spotlist[num3]) == int:
+                        winningMoves.append(board.spotlist[num3])
+        #checks second diagonal
+        for num1,num2,num3 in [2,4,6],[4,6,2],[6,2,4]:
+            if board.spotlist[num1] == board.spotlist[num2] == self.letter and type(board.spotlist[num3]) == int:
+                        winningMoves.append(board.spotlist[num3])
+                        
+        return winningMoves
+    
+    def blockWin(self):
+        blockMoves = []
+        for row in range(3):
+                row *= 3
+                #checks every combination
+                for num1,num2,num3 in [0,1,2],[1,2,0],[2,0,1]:
+                    if board.spotlist[row + num1] == board.spotlist[row + num2] != self.letter and type(board.spotlist[row + num3]) == int:
+                        blockMoves.append(board.spotlist[row + num3])
+        #checks columns
+        for column in range(3):
+            #checks every combination
+            for num1,num2,num3 in [0,3,6],[3,6,0],[6,0,3]:
+                    if board.spotlist[column + num1] == board.spotlist[column + num2] != self.letter and type(board.spotlist[column + num3]) == int:
+                        blockMoves.append(board.spotlist[column + num3])
+        #checks diagonals
+    
+        #checks first diagonal
+        for num1,num2,num3 in [0,4,8],[4,8,0],[8,0,4]:
+            if board.spotlist[num1] == board.spotlist[num2] != self.letter and type(board.spotlist[num3]) == int:
+                        blockMoves.append(board.spotlist[num3])
+        #checks second diagonal
+        for num1,num2,num3 in [2,4,6],[4,6,2],[6,2,4]:
+            if board.spotlist[num1] == board.spotlist[num2] != self.letter and type(board.spotlist[num3]) == int:
+                        blockMoves.append(board.spotlist[num3])
+                        
+        return blockMoves                        
+
+    def corners(self):
+        cornerList = []
+        count = 0
+        for spot in board.spotlist:
+            if count in [0,2,6,8]:
+                if type(spot) is int:
+                    cornerList.append(spot)
+            count += 1
+        return cornerList
+
+    def optimalMove(self):
+        if len(self.winningMove()) > 0:
+            return random.choice(self.winningMove())
+        if len(self.blockWin()) > 0:
+            return random.choice(self.blockWin())
+        if len(self.corners()) > 0:
+            return random.choice(self.corners())
+        return random.choice(board.repCheck()) 
+
+
+#Functions
+def turns(firstP):
+    if player1.letter == 'X':
+        playerX = player1
+        playerO = player2
+    else:
+        playerX = player2
+        playerO = player1 
+
+    t = firstP
+    if t == 1:
+        nextP = 2
+    else:
+        nextP = 1
+    while not board.donePlaying:
+        if t == 1:
+            player1.choiceGetter()
+            player1.choiceToBoard()
+            status = board.endCheck_front(playerX,playerO)
+            t = 2
+            if not status is None:
+            #if someone won, restart the game with player2 going first
+                t = nextP
+                nextP = 1
+                continue
+        if t == 2:
+            player2.choiceGetter()
+            player2.choiceToBoard()
+            status = board.endCheck_front(playerX,playerO)
+            if not status is None:
+            #if someone won, restart the game with player2 going first
+                t = nextP
+                nextP = 2
+            else:
+                t = 1
+                
+def intro():
+    print('Tic Tac Toe\nby Elliot Adam\n\n')
+    while True:
+        try:
+            dif = int(input('Would you like to go play\n1.Player vs Player\n2.Player vs Easy AI\n3.Player vs Hard AI\n'))
+        except:
+            print('\nPlease input 1, 2, or 3')
+            continue
+        if dif not in [1,2,3]:
+            print('Please input a 1, 2, or 3')
+            continue
+        return dif
+
+def starterRequest():
+    while True:
+        starter = input('Who goes first\n1.Player\n2.AI\n3.Random\n')
+        try:
+            starter = int(starter)
+        except:
+            print('Please input 1, 2, or 3')
+        if starter not in [1,2,3]:
+            print('Please input 1, 2, or 3')
+            continue
+        break
+    return starter
+    
+def scorePrint():
+    print("{}'s Score: {} , {}'s Score: {}\n\n".format(player1.name,player1.score,player2.name,player2.score))
+
+#Game Mode Functions
+def playerVPlayer():
+    global player1
+    global player2
+    while True:
+        p1name = input('Player 1, input your name. ')
+        if len(p1name) < 1:
+            print('Insert a name please')
+            continue
+        break
+
+    while True:
+        p2name = input('Player 2, input your name. ')
+        if len(p1name) < 1:
+            print('Insert a name please')
+            continue
+        break
+
+    player1 = Person(p1name,'X')
+    player2 = Person(p2name,'0')
+    turns(1)
+
+def playerVEasyAI():
+    global player1
+    global player2
+    while True:
+        p1name = input('Player 1, input your name. ')
+        if len(p1name) < 1:
+            print('Insert a name please')
+            continue
+        break
+    
+    p2name = 'Computer'
+    
+    pfirst = 0
+    starter = starterRequest()
+    #Randomnly making variable starter 1 or 2 if the user chose random
+    if starter == 3:
+        starter = random.choice([1,2])
+    if starter == 1:
+        p1letter = 'X'
+        p2letter = '0'
+        pfirst = 1
+    if starter == 2:
+        p1letter = '0'
+        p2letter = 'X'
+        pfirst = 2
+    
+    player1 = Person(p1name,p1letter)
+    player2 = EasyAI(p2name,p2letter)
+    turns(pfirst)
+        
+def playerVHardAI():
+    global player1
+    global player2
+    while True:
+        p1name = input('Player 1, input your name. ')
+        if len(p1name) < 1:
+            print('Insert a name please')
+            continue
+        break
+    
+    p2name = 'Hard Computer'
+    
+    pfirst = 0
+    starter = starterRequest()
+    #Randomnly making variable starter 1 or 2 if the user chose random
+    
+    if starter == 3:
+        starter = random.choice([1,2])
+    if starter == 1:
+        p1letter = 'X'
+        p2letter = '0'
+        pfirst = 1
+    if starter == 2:
+        p1letter = '0'
+        p2letter = 'X'
+        pfirst = 2
+    
+    player1 = Person(p1name,p1letter)
+    player2 = HardAI(p2name,p2letter)
+    turns(pfirst)
+        
+#Main Game Code
+if __name__ == '__main__':
+    global board
+    board = Board()
+    board.setBoard()
+    dif = intro()
+    if dif == 1:
+        playerVPlayer()
+    if dif == 2:
+        playerVEasyAI()
+    if dif == 3:
+        playerVHardAI()
